468bdb9c40c1e09f3f15bb0d90be1af88ba149f9
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2023-10-06 18:04:14 +0500

    addchats Page

--- src/components/Chats/index.jsx
+++ src/components/Chats/index.jsx
@@ -2,6 +2,8 @@ import Header from "./Header";
 import ZoomTitle from "./ZoomTitle";
 import { Container } from "./style";
 import Proggram from "./Proggram";
+import Footer from "../Footer";
+import LeaveAplication from "../LeaveAplication";
 
 const Chats = () => {
   return (
@@ -9,6 +11,8 @@ const Chats = () => {
       <Header />
       <ZoomTitle />
       <Proggram />
+      <LeaveAplication />
+      <Footer />
     </Container>
   );
 };
